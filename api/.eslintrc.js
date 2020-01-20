3b88dbc65ff4e18bc180e676f1b211877e0ef646
phoenix-zero <anubhab.mukherjee2013@gmail.com>
2020-01-20 12:21:07 +0530

    Added sendMail

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     env: {
         node: true,
         commonjs: true,
+        es6: true,
     },
     parserOptions: {
         ecmaVersion: 6,
@@ -13,7 +14,7 @@ module.exports = {
     rules: {
         semi: 2,
         quotes: [2, 'single', { avoidEscape: true }],
-        'comma-dangle': [2, 'always'],
+        'comma-dangle': [2, 'always-multiline'],
         'prettier/prettier': 2,
         'brace-style': [2, '1tbs', { allowSingleLine: true }],
     },
