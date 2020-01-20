f0f1f7d06d730b112a464c94eb5f1159433dee51
2bit-hack <sohamkar76@gmail.com>
2020-01-20 16:53:29 +0530

    Added logout and refresh routes

--- api/authServer.js
+++ api/authServer.js
@@ -25,16 +25,24 @@ app.use(express.json());
 app.post('/token', (req, res) => {
     const refreshToken = req.body.token;
     if (refreshToken == null) return res.sendStatus(401);
-    //if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
-        const accessToken = generateAccessToken({ name: user.name });
-        res.json({ accessToken: accessToken });
+    authSchema.findOne({ refreshToken }, (e, res) => {
+        if (e) return res.sendStatus(401);
+        jwt.verify(
+            refreshToken,
+            process.env.REFRESH_TOKEN_SECRET,
+            (err, user) => {
+                if (err) return res.sendStatus(403);
+                const accessToken = generateAccessToken({ name: user.name });
+                authSchema.findOneAndUpdate({ refreshToken }, { accessToken });
+                res.status(200).json({ accessToken: accessToken });
+            },
+        );
     });
 });
 
 app.delete('/logout', (req, res) => {
     //refreshTokens = refreshTokens.filter(token => token !== req.body.token);
+    authSchema.findOneAndDelete({ refreshToken: req.body.token });
     res.sendStatus(204);
 });
 
